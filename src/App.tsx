991de8fd591cc426991a3d301e1a7e2529c69c7b
Alexandru Morarasu <morarasualexandru@gmail.com>
2021-01-14 19:26:22 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let b = 'ceva';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
